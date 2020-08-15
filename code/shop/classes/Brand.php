79639b223530886c13b760634af2547ff950fa1c
AzherUddinTonmoy1712351042 <azher.uddin@northsouth.edu>
2020-08-16 00:02:59 +0600

    Show brand list & Update brand list in admin panel

--- code/shop/classes/Brand.php
+++ code/shop/classes/Brand.php
@@ -35,7 +35,42 @@ class Brand{
 		}
 	}
 	}
-	
+
+    public function getAllBrand(){
+		$query= "SELECT * FROM tbl_brand ORDER BY brandId DESC";
+		$result=$this->db->select($query);
+		return $result;
+
+}
+
+    public function getBrandById($id){
+        $query= "SELECT * FROM tbl_brand WHERE brandId='$id'";//it catId match with ($id) which we sent then it will take.
+		$result=$this->db->select($query);
+		return $result;
+    }
+
+    public function brandUpdate($brandName,$id){
+       $brandName=$this->fm->validation($brandName); //this function will tream space,backslash and secure data. 
+	    $brandName=mysqli_real_escape_string($this->db->link, $brandName);//for avoiding single coutation in string.
+        $id=mysqli_real_escape_string($this->db->link, $id);
+	    If(empty($brandName)){
+		   $msg= "<span class='error'>Brand must not be empty!</span>";
+		   return $msg;
+	     }
+	     else{
+	     	$query="UPDATE tbl_brand
+	     	        SET brandName='$brandName'
+	     	        WHERE brandId='$id'" ;
+	     	$updated_row = $this->db->update($query);
+	     	If($updated_row){
+		   $msg = "<span class='success'>Brand updated successfully.</span>";
+			return $msg;
+	     }else{
+		      $msg="<span class='error'>Brand not Updated successfully.</span>";
+            return $msg;
+	     }
+	     }
+    }
 }
 
 ?>
