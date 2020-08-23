29b05e6182e890a22cb967c3cc0f6ee26ef31974
AzherUddinTonmoy1712351042 <azher.uddin@northsouth.edu>
2020-08-23 12:48:19 +0600

    Edit,Update,Delete product From Admin pannel

--- code/shop/admin/productlist.php
+++ code/shop/admin/productlist.php
@@ -8,11 +8,24 @@ $pd= new Product();
 $fm= new Format();
 
  ?>
+ <?php
+ if (isset($_GET['delpro'])) {
+
+ 	$id = $_GET['delpro'];
+ 	$delpro = $pd->delProById($id);
+ }
+ 
+?>
 
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Post List</h2>
-        <div class="block">  
+        <div class="block"> 
+        <?php
+                	if (isset($delpro)) {
+                		echo "$delpro";
+                	}
+                ?>   
             <table class="data display datatable" id="example">
 			<thead>
 				<tr>
@@ -39,8 +52,8 @@ $fm= new Format();
 				<tr class="odd gradeX">
 					<td><?php echo $i;?></td>
 					<td><?php echo $result['productName'];?></td>
-					<td><?php echo $result['catId'];?></td>
-					<td><?php echo $result['brandId'];?></td>
+					<td><?php echo $result['catName'];?></td>
+					<td><?php echo $result['brandName'];?></td>
 					<td><?php echo $fm->textShorten($result['body'], 50 );?></td>
 					<td>$<?php echo $result['price'];?></td>
 					<td><img src="<?php echo $result['image'];?>" height="40px" width="60px" /></td>
@@ -54,7 +67,7 @@ $fm= new Format();
 					?>
 						
 					</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
+					<td><a href="productedit.php?proid=<?php echo $result['productId'];?>">Edit</a> || <a onclick="return confirm('Are you sure to delete!') " href="?delpro=<?php echo $result['productId'];?>">Delete</a></td>
 				</tr>
 			<?php }} ?>
 				
--- code/shop/classes/Product.php
+++ code/shop/classes/Product.php
@@ -18,11 +18,11 @@ class Product{
 	public function productInsert($data, $file){
 
 		$productName=mysqli_real_escape_string($this->db->link, $data['productName']);
-		$catId=mysqli_real_escape_string($this->db->link, $data['catId']);
-		$brandId=mysqli_real_escape_string($this->db->link, $data['brandId']);
-		$body=mysqli_real_escape_string($this->db->link, $data['body']);
-		$price=mysqli_real_escape_string($this->db->link, $data['price']);
-		$type=mysqli_real_escape_string($this->db->link, $data['type']);
+		$catId      =mysqli_real_escape_string($this->db->link, $data['catId']);
+		$brandId    =mysqli_real_escape_string($this->db->link, $data['brandId']);
+		$body       =mysqli_real_escape_string($this->db->link, $data['body']);
+		$price      =mysqli_real_escape_string($this->db->link, $data['price']);
+		$type       =mysqli_real_escape_string($this->db->link, $data['type']);
 		
 
 		$permited  = array('jpg', 'jpeg', 'png', 'gif');
@@ -65,9 +65,143 @@ class Product{
 
 	}
     public function getAllProduct(){
-    	$query= "SELECT * From tbl_product ORDER BY productId DESC ";
+
+    	$query= "SELECT p.*, c.catName, b.brandName
+    	 FROM tbl_product as p, tbl_category as c, tbl_brand as b
+    	 Where p.catId = c.catId and p.brandId = b.brandId
+         ORDER BY p.productId DESC ";
+    	/*
+    	$query= "SELECT tbl_product.*, tbl_category.catName, tbl_brand.brandName
+    	 FROM tbl_product
+    	 Inner join tbl_category
+    	 on tbl_product.catId = tbl_category.catId
+    	 Inner join tbl_brand
+    	 on tbl_product.brandId = tbl_brand.brandId
+         ORDER BY tbl_product.productId DESC ";
+         */
+
     	$result=$this->db->select($query);
     	return $result;
+    }
+    public function getProById($id){
+    	$query= "SELECT * FROM tbl_product WHERE productId='$id'";
+		$result=$this->db->select($query);
+		return $result;
+    }
+
+    public function productUpdate($data, $file, $id){
+
+		$productName=mysqli_real_escape_string($this->db->link, $data['productName']);
+		$catId      =mysqli_real_escape_string($this->db->link, $data['catId']);
+		$brandId    =mysqli_real_escape_string($this->db->link, $data['brandId']);
+		$body       =mysqli_real_escape_string($this->db->link, $data['body']);
+		$price      =mysqli_real_escape_string($this->db->link, $data['price']);
+		$type       =mysqli_real_escape_string($this->db->link, $data['type']);
+		
+
+		$permited  = array('jpg', 'jpeg', 'png', 'gif');
+
+	    $file_name = $file['image']['name'];
+	    $file_size = $file['image']['size'];
+	    $file_temp = $file['image']['tmp_name'];
+
+    $div = explode('.', $file_name);
+    $file_ext = strtolower(end($div));
+    $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+    $uploaded_image = "uploads/".$unique_image;
+
+    if ($productName == "" || $catId == "" || $brandId == "" || $body == "" || $price == "" || $type == "" ) {
+    	$msg= "<span class='error'>Fields must not be empty!</span>";
+		return $msg;
+    } else{
+           if(!empty($file_name)){
+	    
+	    if ($file_size >1048567) {
+	     echo "<span class='error'>Image Size should be less then 1MB!
+	     </span>";
+	    } 
+	    elseif (in_array($file_ext, $permited) === false) {
+	     echo "<span class='error'>You can upload only:-"
+	     .implode(', ', $permited)."</span>";
+	    } 
+	    else{
+	    	move_uploaded_file($file_temp, $uploaded_image);
+	    	 
+	        $query= "UPDATE tbl_product 
+	                 SET 
+	                  productName='$productName',
+	                  catId      ='$catId',
+	                  brandId    ='$brandId',
+	                  body       ='$body',
+	                  price      ='$price',
+	                  image      ='$uploaded_image',
+	                  type      ='$type'
+	                  WHERE productId='$id' ";
+
+	         $updated_row=$this->db->update($query);
+
+			if($updated_row){
+				$msg = "<span class='success'>product updated successfully.</span>";
+				return $msg;
+			}
+			else{ 
+				$msg="<span class='error'>product not updated successfully.</span>";
+	            return $msg;
+	      
+
+			}
+	    }
+	     }else{
+	    	 
+	        $query= "UPDATE tbl_product 
+	                 SET 
+	                  productName='$productName',
+	                  catId      ='$catId',
+	                  brandId    ='$brandId',
+	                  body       ='$body',
+	                  price      ='$price',
+	                  type       ='$type'
+	                  WHERE productId='$id' ";
+
+	         $updated_row=$this->db->update($query);
+
+			if($updated_row){
+				$msg = "<span class='success'>product updated successfully.</span>";
+				return $msg;
+			}
+			else{ 
+				$msg="<span class='error'>product not updated successfully.</span>";
+	            return $msg;
+	      
+
+			}
+             
+	    }
+
+	  }
+
+	    
+
+    }
+    public function delProById($id){
+    	$query="SELECT * FROM tbl_product where productId='$id' ";
+    	$getData=$this->db->select($query);
+    	if($getData){
+    		while($delImg=$getData->fetch_assoc()){
+    			$dellink=$delImg['image'];
+    			unlink($dellink);
+    		}
+
+    	}
+    	$delquery="DELETE from tbl_product where productId='$id'";
+    	$deldata = $this->db->delete($delquery);
+		if ($deldata) {
+			$msg="<span class='success'>product deleted successfully.</span>";
+            return $msg;
+		}else{
+			$msg="<span class='error'>product not deleted.</span>";
+            return $msg;
+		}
     }
 
 }
